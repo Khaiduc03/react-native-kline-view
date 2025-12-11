4e4fd19cec56e9a995c7f8b0c1e98d8974cb3730
khai <mhdkhai03@gmail.com>
2025-12-11 11:11:03 +0700

    update to typescript

--- example/index.js
+++ example/index.js
@@ -5,5 +5,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import KLineScreen from './screens/KLineScreen';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => KLineScreen);
